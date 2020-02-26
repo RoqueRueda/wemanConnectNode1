c77221753c4ebe6cfd275726f8ce7e8ccc1be3b7
Roque Rueda <skaroque@gmail.com>
2020-02-26 09:56:10 -0600

    say hello world to a client

--- use-npm/index.js
+++ use-npm/index.js
@@ -2,12 +2,12 @@ console.log('Starting index.js');
 const http = require('http');
 const _ = require('lodash');
 
-const server = http.createServer();
-
-server.on('connection', (socket) => {
-    console.log('New connection...');
-})
+const server = http.createServer((req, res) =>{
+    if(req.url === '/') {
+        res.write('Hello World');
+        res.end();
+    }
+});
 
 server.listen('3000');
-
 console.log('Listening on port 3000...');
